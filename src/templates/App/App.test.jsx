01906303c3d943dd453e1c3e05f55fb7fe3c71c9
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-11-08 13:03:02 -0300

    Alterando mediaquery e testando componente Home

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -3,12 +3,8 @@ import { renderTheme } from '../../styles/render-theme'
 import { theme } from '../../styles/theme'
 import Home from '.'
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />)
-  const headingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />)
   })
-  expect(headingContainer).toMatchSnapshot()
-  expect(headingContainer).toHaveStyleRule('background', 'red')
 })
