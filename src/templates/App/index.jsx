dd834d2018f5eff40c25c7d45aed986e4beceaff
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-11-17 14:39:32 -0300

    test

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,14 +1,10 @@
 import { Heading } from '../../components/Heading'
 import * as Styled from './styles'
+import { Base } from '../Base'
+import { mockBase } from '../Base/mock'
 
 function Home() {
-  return (
-    <div className="App">
-      <Styled.Wrapper>
-        <Heading>o texto que eu quero</Heading>
-      </Styled.Wrapper>
-    </div>
-  )
+  return <Base {...mockBase} />
 }
 
 export default Home
