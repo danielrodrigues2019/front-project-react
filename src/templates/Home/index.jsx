bd0c3830026bff802c163e4573e7cae6d398013f
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-12-01 13:34:15 -0300

    show component base in home

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react'
-import { Heading } from '../../components/Heading'
-import { Base } from '../Base'
-import { mockBase } from '../Base/mock'
 import * as Styled from './styles'
 import { mapData } from '../../api/map-data'
+import { Heading } from '../../components/Heading'
+import { GridTwoColum } from '../../components/GridTwoColumn'
+import { GridContent } from '../../components/GridContent'
+import { GridText } from '../../components/GridText'
+import { GridImage } from '../../components/GridImage'
+import { mockBase } from '../Base/mock'
+import { Base } from '../Base'
 import { PageNotFound } from '../PageNotFoud'
 import { Loading } from '../Loading'
 
@@ -33,7 +37,33 @@ function Home() {
     return <Loading />
   }
 
-  return <Base {...mockBase} />
+  const { menu, sections, footerHtml, slug } = data
+  const { links, text, link, srcImg } = menu
+
+  return (
+    <Base links={links} footerHtml={footerHtml} logoData={{ text, link, srcImg }}>
+      {sections.map((section, index) => {
+        const { component } = section
+        const key = `${slug}-${index}`
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColum key={key} {...section} />
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />
+        }
+      })}
+    </Base>
+  )
 }
 
 export default Home
