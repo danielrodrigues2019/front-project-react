4166be3c89e0f5fbb30132d0788be3a6029e192d
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-12-01 13:11:01 -0300

    Create pagenotfound

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Heading } from '../../components/Heading'
+import { Base } from '../Base'
+import { mockBase } from '../Base/mock'
+import * as Styled from './styles'
+import { mapData } from '../../api/map-data'
+
+function Home() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch('http://localhosta:1337/pages/?slug=landing-page')
+        const json = await data.json()
+        const pageData = mapData(json)
+        setData(pageData[0])
+      } catch (e) {
+        setData(undefined)
+      }
+    }
+
+    load()
+  }, [])
+
+  if (data === undefined) {
+    return <h1>Página não encontrada</h1>
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>
+  }
+
+  return <Base {...mockBase} />
+}
+
+export default Home
